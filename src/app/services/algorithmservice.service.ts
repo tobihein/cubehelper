43d2a6d3053143a31c60d3c65120fccd1bbd99ae
Tobias Hein <th.mailinglists@googlemail.com>
2020-09-13 22:11:00 +0200

    Added details for algorithms

--- src/app/services/algorithmservice.service.ts
+++ src/app/services/algorithmservice.service.ts
@@ -9,15 +9,16 @@ export class AlgorithmserviceService {
   constructor() { }
 
   public getAlgorithms(): Algorithmmodel[] {
-    const alg1 = this.createAlgorithm('1', 'first');
-    const alg2 = this.createAlgorithm('2', 'second');
+    const alg1 = this.createAlgorithm('1', 'first',  ['R' , 'U\'', 'L', 'D']);
+    const alg2 = this.createAlgorithm('2', 'second', ['L', 'R\'', 'D', 'L\'']);
     return [alg1, alg2 ];
   }
 
-  private createAlgorithm(id: string, name: string): Algorithmmodel {
+  private createAlgorithm(id: string, name: string, moves: string[]): Algorithmmodel {
     const alg = new Algorithmmodel();
     alg.setId(id);
     alg.setName(name);
+    alg.setMoves(moves);
     return alg;
   }
 }
