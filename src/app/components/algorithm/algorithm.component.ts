6df48cf9a3c220a3ec5bca40168924d3baa88ac1
Tobias Hein <th.mailinglists@googlemail.com>
2020-10-11 23:07:44 +0200

    More algorithms and possibility to invert algorithm

--- src/app/components/algorithm/algorithm.component.ts
+++ src/app/components/algorithm/algorithm.component.ts
@@ -1,4 +1,5 @@
 import {Component, Input, OnInit} from '@angular/core';
+import { AlgorithmserviceService } from 'src/app/services/algorithmservice.service';
 import {Algorithmmodel} from '../../models/algorithmmodel';
 import {MoveComponent} from '../move/move.component';
 
@@ -9,13 +10,25 @@ import {MoveComponent} from '../move/move.component';
 })
 export class AlgorithmComponent implements OnInit {
   @Input() private algorithm: Algorithmmodel;
-
-  constructor() { }
+  private showInverted: boolean;
+  
+  constructor(private algorithmService: AlgorithmserviceService) { }
 
   ngOnInit(): void {
   }
 
   public getAlgorithm(): Algorithmmodel{
     return this.algorithm;
+  }
+
+  public getInvertedAlgorithm(): string[] {
+    return this.algorithmService.invertAlgorithm(this.algorithm.getId());
+  }
+
+  public toggle(): void {
+    this.showInverted=!this.showInverted;
+  }
+  public show(): boolean {
+    return this.showInverted;
   }
 }
--- src/app/services/algorithmservice.service.ts
+++ src/app/services/algorithmservice.service.ts
@@ -5,11 +5,9 @@ import {Algorithmmodel} from '../models/algorithmmodel';
   providedIn: 'root'
 })
 export class AlgorithmserviceService {
+  private algorithms: Algorithmmodel[];
 
   constructor() {
-  }
-
-  public getAlgorithms(): Algorithmmodel[] {
     const tperm = this.createAlgorithm('tperm', 'T-Perm', 'R U R\' U\' R\' F R R U\' R\' U\' R U R\' F\'');
     const yperm = this.createAlgorithm('yperm', 'Y-Perm', 'F R U\' R\' U\' R U R\' F\' R U R\' U\' R\' F R F\'');
     const jpermRight = this.createAlgorithm('jperm_right', 'J-Perm_Right', 'R U R\' F\' R U R\' U\' R\' F R R U\' R\' U\'');
@@ -17,8 +15,8 @@ export class AlgorithmserviceService {
     const eperm = this.createAlgorithm('eperm', 'E-Perm', 'L U\' R D D R\' U L\' R U\' L D D L\' U R\'');
     const aperm1 = this.createAlgorithm('aperm1', 'A-Perm 1', '');
     const aperm2 = this.createAlgorithm('aperm2', 'A-Perm 2', null);
-    const fperm = this.createAlgorithm('fperm', 'F-Perm');
-    const vperm = this.createAlgorithm('vperm', 'V-Perm');
+    const fperm = this.createAlgorithm('fperm', 'F-Perm', 'R\' U\' F\' R U R\' U\' R\' F R2 U\' R\' U\' R U R\' U R');
+    const vperm = this.createAlgorithm('vperm', 'V-Perm', 'R\' U R\' Dw\' R\' F\' R2 U\' R\' U R\' F R F');
     const gperm1 = this.createAlgorithm('gperm1', 'G-Perm 1');
     const gperm2 = this.createAlgorithm('gperm2', 'G-Perm 2');
     const gperm3 = this.createAlgorithm('gperm3', 'G-Perm 3');
@@ -31,7 +29,16 @@ export class AlgorithmserviceService {
     const nperm1 = this.createAlgorithm('nperm1', 'N-Perm 1');
     const nperm2 = this.createAlgorithm('nperm2', 'N-Perm 2');
     const zperm = this.createAlgorithm('zperm', 'Z-Perm');
-    return [tperm, yperm, jpermRight, jpermLeft, eperm, zperm, vperm, aperm1, aperm2, fperm, gperm1, gperm2, gperm3, gperm4, hperm, rpermLeft, rpermRight, upermLeft, upermRight, nperm1, nperm2];
+    this.algorithms = [tperm, yperm, jpermRight, jpermLeft, eperm, zperm, vperm, aperm1, aperm2, fperm, gperm1, gperm2, gperm3, gperm4, hperm, rpermLeft, rpermRight, upermLeft, upermRight, nperm1, nperm2];
+  }
+
+  public getAlgorithms(): Algorithmmodel[] {
+    return this.algorithms;
+  }
+
+  public invertAlgorithm(id: string) : string[] {
+    const org = this.algorithms.find(alg => alg.getId() == id);
+    return this.invert(org.getMoves());
   }
 
   private createAlgorithm(id: string, name: string, moves = ''): Algorithmmodel {
@@ -44,5 +51,17 @@ export class AlgorithmserviceService {
       alg.setMoves([]);
     }
     return alg;
+  }
+
+  private invert(moves: string[]): string[] {
+    const inverted: string[] = [];
+    moves.forEach ( m => {
+      if(m.endsWith('\'')) {
+        inverted.push(m.substring(0, m.length-1))
+      } else {
+        inverted.push(m + '\'');
+      }
+    });
+    return inverted;
   }
 }
